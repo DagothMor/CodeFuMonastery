7a7a091de438e89cec06a823bb5273aa2c0b87eb
Alexander <siegfried1997@gmail.com>
2022-04-22 20:08:20 +0400

    BalancedRoundBrackets realization

---
+++ StackController.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CodeFuMonastery
+{
+	public static class StackController
+	{
+		public static bool BalancedRoundBrackets(string stringin)
+		{
+			var stack = new AlgorithmsDataStructures.Stack<char>();
+			foreach (var bracket in stringin)
+			{
+				if (bracket == ')')
+				{
+					if (stack.Peek() == default)
+						return false;
+					stack.Pop();
+					continue;
+				}
+				stack.Push(bracket);
+			}
+			return stack.Size() == 0;
+		}
+	}
+}
