40928a0ec7f1cd75ce4be1afbcefa1b50fcb84aa
Alexander <siegfried1997@gmail.com>
2022-04-22 20:37:38 +0400

    PostfixNotationOfAnExpression realization

--- StackController.cs
+++ StackController.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace CodeFuMonastery
 {
 	public static class StackController
@@ -19,5 +21,39 @@ namespace CodeFuMonastery
 			}
 			return stack.Size() == 0;
 		}
+
+		public static int PostfixNotationOfAnExpression(string stringin)
+		{
+			//var s1 = new AlgorithmsDataStructures.Stack<int>();
+			var s2 = new AlgorithmsDataStructures.Stack<int>();
+
+
+			foreach (var ch in stringin)
+			{
+				if (char.IsDigit(ch))
+				{
+					s2.Push(Int32.Parse(ch.ToString()));
+				}
+
+				if (ch == '+')
+				{
+					s2.Push(sum(s2.Pop(),s2.Pop()));
+				}
+
+				if (ch == '*')
+				{
+					s2.Push(multiply(s2.Pop(), s2.Pop()));
+				}
+
+				if (ch == '=')
+				{
+					return s2.Peek();
+				}
+			}
+			return s2.Peek();
+			
+		}
+		public static int sum(int a, int b) => a + b;
+		public static int multiply(int a, int b) => a * b;
 	}
 }
