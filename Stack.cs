0ce152f7e26a5965ef8ad285b94cb6978ec01ece
Alexander <siegfried1997@gmail.com>
2022-04-22 20:39:32 +0400

    hotfix

--- Stack.cs
+++ Stack.cs
@@ -4,37 +4,37 @@ using System.Collections.Generic;
 namespace AlgorithmsDataStructures
 {
 
-    public class Stack<T>
-    {
+	public class Stack<T>
+	{
 		readonly LinkedList<T> stack = new LinkedList<T>();
-        public Stack()
-        {
-            stack = new LinkedList<T>();
-        }
+		public Stack()
+		{
+			stack = new LinkedList<T>();
+		}
 
-        public int Size()
-        {
-            return stack.Count;
-        }
+		public int Size()
+		{
+			return stack.Count;
+		}
 
-        public T Pop()
-        {
-            if(stack.Count == 0) return default(T);
-            var node = stack.First;
-            stack.RemoveFirst();
-            return node.Value;
-        }
+		public T Pop()
+		{
+			if (stack.Count == 0) return default(T);
+			var node = stack.First;
+			stack.RemoveFirst();
+			return node.Value;
+		}
 
-        public void Push(T val)
-        {
-            stack.AddLast(val);
-        }
+		public void Push(T val)
+		{
+			stack.AddLast(val);
+		}
 
-        public T Peek()
-        {
-            if (stack.Count == 0) return default(T);
-            return stack.First.Value;
-        }
-    }
+		public T Peek()
+		{
+			if (stack.Count == 0) return default(T);
+			return stack.First.Value;
+		}
+	}
 
 }
