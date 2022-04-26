e2c01d0e1614710afc8d8f878ee537dba3f91b1c
siegfried1997 <siegfried1997@gmail.com>
2022-04-26 18:41:24 +0400

    Fixing stack controller added variable order

--- StackController.cs
+++ StackController.cs
@@ -3,108 +3,117 @@ using System;
 
 namespace CodeFuMonastery
 {
-	public static class StackController
-	{
-		public static bool BalancedRoundBrackets(string stringin)
-		{
-			var stack = new AlgorithmsDataStructures.Stack<char>();
-			foreach (var bracket in stringin)
-			{
-				if (bracket == ')')
-				{
-					if (stack.Size() == 0)
-						return false;
-					stack.Pop();
-					continue;
-				}
-				stack.Push(bracket);
-			}
-			return stack.Size() == 0;
-		}
+    public static class StackController
+    {
+        public static bool BalancedRoundBrackets(string stringin)
+        {
+            var stack = new AlgorithmsDataStructures.Stack<char>();
+            foreach (var bracket in stringin)
+            {
+                if (bracket == ')')
+                {
+                    if (stack.Size() == 0)
+                        return false;
+                    stack.Pop();
+                    continue;
+                }
+                stack.Push(bracket);
+            }
+            return stack.Size() == 0;
+        }
 
-		public static float PostfixNotationOfAnExpressionThroughString(string stringin)
-		{
-			var s2 = new AlgorithmsDataStructures.Stack<float>();
+        public static float PostfixNotationOfAnExpressionThroughString(string stringin)
+        {
+            var s2 = new AlgorithmsDataStructures.Stack<float>();
 
 
-			foreach (var ch in stringin)
-			{
-				if (char.IsDigit(ch))
-				{
-					s2.Push(float.Parse(ch.ToString()));
-				}
+            foreach (var ch in stringin)
+            {
+                if (char.IsDigit(ch))
+                {
+                    s2.Push(float.Parse(ch.ToString()));
+                }
 
-				if (ch == '+')
-				{
-					s2.Push(sum(s2.Pop(),s2.Pop()));
-				}
+                if (ch == '+')
+                {
+                    s2.Push(sum(s2.Pop(), s2.Pop()));
+                }
 
-				if (ch == '-')
-				{
-					s2.Push(sub(s2.Pop(), s2.Pop()));
-				}
+                if (ch == '-')
+                {
+                    s2.Push(sub(s2.Pop(), s2.Pop()));
+                }
 
-				if (ch == '/')
-				{
-					s2.Push(div(s2.Pop(), s2.Pop()));
-				}
+                if (ch == '/')
+                {
+                    s2.Push(div(s2.Pop(), s2.Pop()));
+                }
 
-				if (ch == '*')
-				{
-					s2.Push(multiply(s2.Pop(), s2.Pop()));
-				}
+                if (ch == '*')
+                {
+                    s2.Push(multiply(s2.Pop(), s2.Pop()));
+                }
 
-				if (ch == '=')
-				{
-					return s2.Peek();
-				}
-			}
-			return s2.Peek();
-			
-		}
+                if (ch == '=')
+                {
+                    return s2.Peek();
+                }
+            }
+            return s2.Peek();
 
-		public static float PostfixNotationOfAnExpression(AlgorithmsDataStructures.Stack<char> stackin)
-		{
-			var s2 = new AlgorithmsDataStructures.Stack<float>();
+        }
 
-			char buffer;
+        public static float PostfixNotationOfAnExpression(AlgorithmsDataStructures.Stack<char> stackin)
+        {
+            var s2 = new AlgorithmsDataStructures.Stack<float>();
 
-			while (stackin.Size() > 0 ) {
-				buffer = stackin.Pop();
-				if (char.IsDigit(buffer))
-				{
-					s2.Push(float.Parse(buffer.ToString()));
-				}
+            char buffer;
+            
+            // for variable order
+            float first, second;
 
-				if (buffer == '+')
-				{
-					s2.Push(sum(s2.Pop(), s2.Pop()));
-				}
+            while (stackin.Size() > 0)
+            {
+                buffer = stackin.Pop();
+                if (char.IsDigit(buffer))
+                {
+                    s2.Push(float.Parse(buffer.ToString()));
+                }
 
-				if (buffer == '-')
-				{
-					s2.Push(sub(s2.Pop(), s2.Pop()));
-				}
-				if (buffer == '*')
-				{
-					s2.Push(multiply(s2.Pop(), s2.Pop()));
-				}
-				if (buffer == '/')
-				{
-					s2.Push(div(s2.Pop(), s2.Pop()));
-				}
+                if (buffer == '+')
+                {
+                    s2.Push(sum(s2.Pop(), s2.Pop()));
+                }
 
-				if (buffer == '=')
-				{
-					return s2.Peek();
-				}
-			}
-			return s2.Peek();
+                if (buffer == '-')
+                {
+                    second = s2.Pop();
+                    first = s2.Pop();
+                    s2.Push(sub(first, second));
+                }
 
-		}
-		public static float sum(float a, float b) => a + b;
-		public static float sub(float a, float b) => b - a;
-		public static float multiply(float a, float b) => a * b;
-		public static float div(float a, float b) => b / a;
-	}
+                if (buffer == '*')
+                {
+                    s2.Push(multiply(s2.Pop(), s2.Pop()));
+                }
+                if (buffer == '/')
+                {
+                    second = s2.Pop();
+                    first = s2.Pop();
+                    s2.Push(div(first, second));
+                }
+
+                if (buffer == '=')
+                {
+                    return s2.Peek();
+                }
+            }
+            return s2.Peek();
+
+        }
+        public static float sum(float a, float b) => a + b;
+        public static float sub(float a, float b) => a - b;
+        public static float multiply(float a, float b) => a * b;
+        public static float div(float a, float b) => a / b;
+    }
 }
