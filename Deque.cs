a03f2af3d89104bc393bf70845d2f11e75bd63de
Alexander <siegfried1997@gmail.com>
2022-04-30 02:38:27 +0400

    Added Deque

---
+++ Deque.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+	public class DummyDequeNode<T> : DequeNode<T>
+	{
+		public DummyDequeNode(T _value) : base(_value)
+		{
+			value = _value;
+		}
+		public DummyDequeNode() : base()
+		{
+			value = default;
+		}
+
+	}
+	public class DequeNode<T>
+	{
+		public T value;
+		public DequeNode<T> next, prev;
+
+		public DequeNode(T _value)
+		{
+			value = _value;
+			next = null;
+			prev = null;
+		}
+		public DequeNode()
+		{
+			value = default;
+			next = null;
+			prev = null;
+		}
+	}
+	public class Deque<T>
+	{
+		private DummyDequeNode<T> dummyHead = new DummyDequeNode<T>();
+		private DummyDequeNode<T> dummyTail = new DummyDequeNode<T>();
+
+		public DummyDequeNode<T> head;
+		public DummyDequeNode<T> tail;
+		public Deque()
+		{
+			head = dummyHead;
+			tail = dummyTail;
+			head.next = tail;
+			head.prev = tail;
+			tail.prev = head;
+			tail.next = head;
+		}
+
+		public void AddFront(T item)
+		{
+			var node = new DequeNode<T>(item);
+			head.next.prev = node;
+			node.next = head.next;
+			node.prev = head;
+			head.next = node;
+			
+		}
+
+		public void AddTail(T item)
+		{
+			var node = new DequeNode<T>(item);
+			tail.prev.next = node;
+			node.prev = tail.prev;
+			node.next = tail;
+			tail.prev = node;
+			
+		}
+
+		public T RemoveFront()
+		{
+			var node = head.next;
+			if (node is DummyDequeNode<T>) return default(T);
+			node.next.prev = head;
+			head.next = node.next;
+			return node.value;
+		}
+
+		public T RemoveTail()
+		{
+			var node = tail.prev;
+			if (node is DummyDequeNode<T>) return default(T);
+			node.prev.next = tail;
+			tail.prev = node.prev;
+			return node.value;
+		}
+
+		public int Size()
+		{
+			var node = head.next;
+			int count = 0;
+			while (!(node is DummyDequeNode<T>))
+			{
+				count++;
+				node = node.next;
+			}
+			return count;
+		}
+	}
+
+}
---
+++ DequeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmsDataStructures;
+
+namespace CodeFuMonastery
+{
+	public static class DequeController
+	{
+		public static bool IsPalindrome(string word)
+		{
+			var deque = StringToDeque(word);
+			char head, tail;
+			while (deque.Size() > 1) 
+			{
+				head = deque.RemoveFront();
+				tail = deque.RemoveTail();
+				if (head != tail) return false;
+			}
+			return true;
+		}
+		private static Deque<char> StringToDeque(string word)
+		{
+			var deque = new Deque<char>();
+			foreach (var c in word) { deque.AddTail(c); }
+			return deque;
+		}
+	}
+}
