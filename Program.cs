82887f8d9c84f45e65ea290699d918a1aa019efc
siegfried1997 <siegfried1997@gmail.com>
2022-10-12 01:00:36 +0400

    Recursion: task 3 and 4

--- Program.cs
+++ Program.cs
@@ -7,27 +7,38 @@ using System.Threading.Tasks;
 
 namespace AlgorithmsDataStructures
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			//         Console.WriteLine(Recursion.Exponentiation(1, 1));
-			//Console.WriteLine(Recursion.Exponentiation(2, 1));
-			//Console.WriteLine(Recursion.Exponentiation(2, 2));
-			//Console.WriteLine(Recursion.Exponentiation(2, 4));
-			//Console.WriteLine(Recursion.Exponentiation(3, 2));
-			//Console.WriteLine(Recursion.Exponentiation(3, 3));
-			//Console.WriteLine(Recursion.Exponentiation(0, 1));
-			//Console.WriteLine(Recursion.Exponentiation(0, 0));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("0"));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("1"));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("2"));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("123"));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("1234"));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("1235"));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("999"));
-			Console.WriteLine(Recursion.SumOfDigitsInNumber("333"));
-			Console.ReadKey();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //         Console.WriteLine(Recursion.Exponentiation(1, 1));
+            //Console.WriteLine(Recursion.Exponentiation(2, 1));
+            //Console.WriteLine(Recursion.Exponentiation(2, 2));
+            //Console.WriteLine(Recursion.Exponentiation(2, 4));
+            //Console.WriteLine(Recursion.Exponentiation(3, 2));
+            //Console.WriteLine(Recursion.Exponentiation(3, 3));
+            //Console.WriteLine(Recursion.Exponentiation(0, 1));
+            //Console.WriteLine(Recursion.Exponentiation(0, 0));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("0"));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("1"));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("2"));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("123"));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("1234"));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("1235"));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("999"));
+            //Console.WriteLine(Recursion.SumOfDigitsInNumber("333"));
+
+            //var stack1 = new System.Collections.Generic.Stack<int>();
+
+            //Console.WriteLine(Recursion.LengthOfList<int>(stack1));
+
+            Console.WriteLine(Recursion.IsPalyndrome("010"));
+            Console.WriteLine(Recursion.IsPalyndrome("0120"));
+            Console.WriteLine(Recursion.IsPalyndrome("012"));
+            Console.WriteLine(Recursion.IsPalyndrome("01010"));
+            Console.WriteLine(Recursion.IsPalyndrome("abcdedcba"));
+
+            Console.ReadKey();
+        }
+    }
 }
--- Recursion/Recursion.cs
+++ Recursion/Recursion.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CodeFuMonastery.Recursion
 {
 
@@ -24,5 +25,26 @@ namespace CodeFuMonastery.Recursion
             if (number.Length == 0) return 0;
             return int.Parse(number[0].ToString()) + SumOfDigitsInNumber(number.Substring(1, number.Length - 1));
         }
+
+        // расчёт длины списка, для которого
+        // разрешена только операция удаления первого элемента pop(0)
+        // (и получение длины конечно);  
+        public static int LengthOfList<T>(Stack<T> stack)
+        {
+            if (stack.Count == 0) return 0;
+            stack.Pop();
+            return 1 + LengthOfList(stack);
+        }
+        // проверка, является ли строка палиндромом;
+        public static bool IsPalyndrome(string text)
+        {
+            if (text.Count() == 3) return text[0] == text[text.Length - 1];
+            if (text.Count() == 2) return false;
+            if (text.Count() == 1) return true;
+            if (text[0] == text[text.Length - 1])
+                return IsPalyndrome(text.Substring(1, text.Length - 2));
+            return false;
+        }
+
     }
 }
