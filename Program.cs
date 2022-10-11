9061777eaf0f89c641e9ee7d5d5915f0261420f5
siegfried1997 <siegfried1997@gmail.com>
2022-10-11 21:57:14 +0400

    Recursion: 1 and 2 task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeFuMonastery.Recursion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,23 @@ namespace AlgorithmsDataStructures
 	{
 		static void Main(string[] args)
 		{
-
+			//         Console.WriteLine(Recursion.Exponentiation(1, 1));
+			//Console.WriteLine(Recursion.Exponentiation(2, 1));
+			//Console.WriteLine(Recursion.Exponentiation(2, 2));
+			//Console.WriteLine(Recursion.Exponentiation(2, 4));
+			//Console.WriteLine(Recursion.Exponentiation(3, 2));
+			//Console.WriteLine(Recursion.Exponentiation(3, 3));
+			//Console.WriteLine(Recursion.Exponentiation(0, 1));
+			//Console.WriteLine(Recursion.Exponentiation(0, 0));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("0"));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("1"));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("2"));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("123"));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("1234"));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("1235"));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("999"));
+			Console.WriteLine(Recursion.SumOfDigitsInNumber("333"));
+			Console.ReadKey();
 		}
 	}
 }
---
+++ Recursion/Recursion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFuMonastery.Recursion
+{
+    
+    public static class Recursion
+    {
+        // возведение числа N в степень M;
+        public static int Exponentiation( int number, int power)
+        {
+            if (power == 0 || number == 1) return 1;
+            if(power == 1) return number;
+            power -= 1;
+            return number * Exponentiation(number, power);
+        }
+
+        // вычисление суммы цифр числа;
+        public static int SumOfDigitsInNumber(string number) 
+        {
+            if (number.Length == 0) return 0;
+            return int.Parse(number[0].ToString()) + SumOfDigitsInNumber(number.Substring(1,number.Length-1));
+        }
+    }
+}
