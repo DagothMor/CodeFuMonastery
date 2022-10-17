c6c33cfc27318b3e730c8154bc5efdcc0e6141dc
siegfried1997 <siegfried1997@gmail.com>
2022-10-18 01:52:16 +0400

    SimpleTree Test

--- Program.cs
+++ Program.cs
@@ -11,53 +11,7 @@ namespace AlgorithmsDataStructures
     {
         static void Main(string[] args)
         {
-            //         Console.WriteLine(Recursion.Exponentiation(1, 1));
-            //Console.WriteLine(Recursion.Exponentiation(2, 1));
-            //Console.WriteLine(Recursion.Exponentiation(2, 2));
-            //Console.WriteLine(Recursion.Exponentiation(2, 4));
-            //Console.WriteLine(Recursion.Exponentiation(3, 2));
-            //Console.WriteLine(Recursion.Exponentiation(3, 3));
-            //Console.WriteLine(Recursion.Exponentiation(0, 1));
-            //Console.WriteLine(Recursion.Exponentiation(0, 0));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("0"));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("1"));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("2"));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("123"));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("1234"));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("1235"));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("999"));
-            //Console.WriteLine(Recursion.SumOfDigitsInNumber("333"));
-
-            //var stack1 = new System.Collections.Generic.Stack<int>();
-
-            //Console.WriteLine(Recursion.LengthOfList<int>(stack1));
-
-            //Console.WriteLine(Recursion.IsPalyndrome("010"));
-            //Console.WriteLine(Recursion.IsPalyndrome("0120"));
-            //Console.WriteLine(Recursion.IsPalyndrome("012"));
-            //Console.WriteLine(Recursion.IsPalyndrome("01010"));
-            //Console.WriteLine(Recursion.IsPalyndrome("abcdedcba"));
-            //Console.WriteLine(Recursion.IsPalyndrome("abba"));
-            //var list = new List<int>() {1,1,2,4,6,1,1,8,8,10,19,1,2 };
-            //var listEvenIndexes = new List<int>() { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
-            //Recursion.WriteOnlyEvenValuesFromList(list,0);
-            //Recursion.WriteOnlyEvenIndexesFromList(listEvenIndexes,0);
-            //var list = new List<int>() {1,2,3,12,13};
-            //Console.WriteLine(Recursion.FindSecondMaxNumber(list)); 
-
-            //var files = Recursion.FindFiles(@"C:\testFolder\", "redkey.txt");
-            //foreach (var item in files)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            int countOfOpenBrackets = 4;
-
-            Recursion.GenerateAllVariantsOfBalanceBrackets(countOfOpenBrackets);
-
             Console.ReadKey();
-
-
         }
     }
 }
---
+++ SimpleTree.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class SimpleTreeNode<T>
+    {
+        public T NodeValue; // значение в узле
+        public SimpleTreeNode<T> Parent; // родитель или null для корня
+        public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+        public int Level;
+
+        public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
+        {
+            NodeValue = val;
+            Parent = parent;
+            Children = null;
+            Level = 0;
+        }
+    }
+
+    public class SimpleTree<T>
+    {
+        public SimpleTreeNode<T> Root; // корень, может быть null
+
+        public SimpleTree(SimpleTreeNode<T> root)
+        {
+            Root = root;
+            Root.Level = 0;
+        }
+
+        // Done.
+        public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+        {
+            if (ParentNode == null || NewChild == null) return;
+            ParentNode.Children.Add(NewChild);
+            NewChild.Parent = ParentNode;
+        }
+
+        // Done.
+        public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
+        {
+            if (NodeToDelete == null) return;
+            var parentNode = NodeToDelete.Parent;
+            if (parentNode == null) return;
+
+            parentNode.Children.Remove(NodeToDelete);
+            NodeToDelete.Parent = null;
+        }
+
+        // Done.
+        public List<SimpleTreeNode<T>> GetAllNodes()
+        {
+            return BFS(this.Root);
+        }
+
+        
+        public List<SimpleTreeNode<T>> BFS(SimpleTreeNode<T> currentNode)
+        {
+            var listOfNodes = new List<SimpleTreeNode<T>>();
+            foreach (var node in currentNode.Children)
+            {
+                listOfNodes.Add(node);
+            }
+            foreach (var node in currentNode.Children)
+            {
+                listOfNodes.AddRange(BFS(node));
+            }
+            return listOfNodes;
+        }
+
+        // Done.
+        public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+        {
+            return BFSNodesByValue(this.Root, val);
+        }
+
+        public List<SimpleTreeNode<T>> BFSNodesByValue(SimpleTreeNode<T> currentNode,T val)
+        {
+            var listOfNodes = new List<SimpleTreeNode<T>>();
+            foreach (var node in currentNode.Children)
+            {
+                if(node.NodeValue.Equals(val))
+                listOfNodes.Add(node);
+            }
+            foreach (var node in currentNode.Children)
+            {
+                listOfNodes.AddRange(BFSNodesByValue(node, val));
+            }
+            return listOfNodes;
+        }
+
+        //Done.
+        public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+        {
+            // ваш код перемещения узла вместе с его поддеревом -- 
+            // в качестве дочернего для узла NewParent
+            var parentOfOriginalNode = OriginalNode.Parent;
+            parentOfOriginalNode.Children.Remove(OriginalNode);
+            OriginalNode.Parent = NewParent;
+            NewParent.Children.Add(OriginalNode);
+        }
+
+        // Done.
+        public int Count()
+        {
+            return BFSCount(this.Root);
+        }
+
+        public int BFSCount(SimpleTreeNode<T> currentNode)
+        {
+            var count = 0;
+            foreach (var node in currentNode.Children)
+            {
+                count++;
+            }
+            foreach (var node in currentNode.Children)
+            {
+                count += BFSCount(node);
+            }
+            return count;
+        }
+
+        //Done
+        public int LeafCount()
+        {
+            return DFSLeafCount(this.Root);
+        }
+        public int DFSLeafCount(SimpleTreeNode<T> currentNode)
+        {
+            if (currentNode.Children.Count == 0)
+            {
+                return 1;
+            }
+            var count = 0;
+            foreach (var node in currentNode.Children)
+            {
+                count += DFSLeafCount(node);
+            }
+            return count;
+        }
+
+        // Done.
+        public void AddLevelToAllNodes() 
+        {
+            BFSAddLevelToAllNodes(this.Root,1);
+        }
+        public void BFSAddLevelToAllNodes(SimpleTreeNode<T> currentNode,int level)
+        {
+            foreach (var node in currentNode.Children)
+            {
+                node.Level = level;
+            }
+            foreach (var node in currentNode.Children)
+            {
+                BFSAddLevelToAllNodes(node, level+1);
+            }
+        }
+    }
+
+}
