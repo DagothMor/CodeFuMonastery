48062c4e0e407af126d94e3aa3d73075dc22f0ed
siegfried1997 <siegfried1997@gmail.com>
2022-11-01 17:40:48 +0400

    GenerateBBSTArray

---
+++ BalancedBST.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            Array.Sort(a);
+            var balancedList = new List<int>();
+            var tree = new BST<int>(GenerateBalancedTree(a));
+            var arrayOfNodes = tree.WideAllNodes();
+            foreach (var node in arrayOfNodes)
+            {
+                balancedList.Add(node.NodeKey);
+            }
+            return balancedList.ToArray();
+        }
+
+        public static BSTNode<int> GenerateBalancedTree(int[] arr)
+        {
+            return sortedArrayToBST(arr, 0, arr.Length - 1, null);
+        }
+
+        public static BSTNode<int> sortedArrayToBST(int[] arr, int start, int end, BSTNode<int> currentNode)
+        {
+            if (start > end) return null;
+            int mid = (start + end) / 2;
+            BSTNode<int> newNode = new BSTNode<int>(arr[mid], 0, currentNode);
+            newNode.LeftChild = sortedArrayToBST(arr, start, mid - 1, newNode);
+            newNode.RightChild = sortedArrayToBST(arr, mid + 1, end, newNode);
+            return newNode;
+        }
+
+    }
+}
