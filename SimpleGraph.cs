d1bb1eb03368e544a4054e09111c9fa582f915c6
siegfried1997 <siegfried1997@gmail.com>
2022-11-11 21:27:58 +0400

    Added SimpleGraph

---
+++ SimpleGraph.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Vertex
+    {
+        public int Value;
+        public Vertex(int val)
+        {
+            Value = val;
+        }
+    }
+
+    public class SimpleGraph
+    {
+        public Vertex[] vertex;
+        public int[,] m_adjacency;
+        public int max_vertex;
+
+        public SimpleGraph(int size)
+        {
+            max_vertex = size;
+            m_adjacency = new int[size, size];
+            vertex = new Vertex[size];
+        }
+
+        public void AddVertex(int value)
+        {
+            for (int i = 0; i < max_vertex; i++)
+            {
+                if (vertex[i] == null)
+                {
+                    var newVertex = new Vertex(value);
+                    vertex[i] = newVertex;
+                    return;
+                }
+            }
+        }
+
+        public void RemoveVertex(int v)
+        {
+            vertex[v] = null;
+            for (int i = 0; i < max_vertex; i++)
+            {
+                RemoveEdge(v, i);
+            }
+        }
+
+        public bool IsEdge(int v1, int v2)
+        {
+            if (m_adjacency[v1, v2] == 1 || m_adjacency[v2, v1] == 1) return true;
+            return false;
+        }
+
+        public void AddEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 1;
+            m_adjacency[v2, v1] = 1;
+        }
+
+        public void RemoveEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 0;
+            m_adjacency[v2, v1] = 0;
+        }
+    }
+}
